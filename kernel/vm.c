96931b6f3f66e3f6a22c8b67d6a80b022b52542e
pipicaca123 <u9593103@gmail.com>
2024-12-29 11:50:09 +0800

    Print a page table done

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+static void vmlevelprintf(pagetable_t pagetable, uint8 pagetable_level){
+  uint8 next_pt_level = pagetable_level + 1;
+  
+  for(int idx = 0; idx < 512; idx++){
+    pte_t pte = pagetable[idx];
+    if((pte & PTE_V)){
+      for(int i = 0; i < pagetable_level; i++)
+        printf(".. ");
+      printf("..%d: pte %p pa %p\n", idx, pte, PTE2PA(pte));
+      if((pte & (PTE_W | PTE_R | PTE_X)) == 0){
+        // this PTE points to a lower-level page table.
+        uint64 child_pte = PTE2PA(pte);
+        vmlevelprintf((pagetable_t)child_pte, next_pt_level); 
+      }else{
+        // leaf page
+      }
+    }
+  }
+}
+
+void vmprintf(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  vmlevelprintf(pagetable, 0);
+}
